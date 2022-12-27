6fc361d39437709dcf7b605adbe73bba7d7d0911
grokhi <grokhael@gmail.com>
2022-12-27 12:50:33 +0400

    upl abt_test csv

--- ab_tests/ab_test.py
+++ ab_tests/ab_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import mannwhitneyu, ttest_ind
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -11,44 +12,37 @@ from airflow.operators.python_operator import PythonOperator
 #     'owner': 'airflow',
 # }
 
+url = 'https://raw.githubusercontent.com/grokhi/startMLapp/main/ab_tests/tmp_agg.csv'
+
 def load_df(ti):
     """
     Gets totalTestResultsIncrease field from Covid API for given state and returns value
     """
-    res = requests.get(url + '{0}/current.json'.format(state))
-    testing_increase = json.loads(res.text)['totalTestResultsIncrease']
-    # в ti уходит task_instance, его передает Airflow под таким названием
-    # когда вызывает функцию в ходе PythonOperator
+    df = pd.read_csv(url)
+
     ti.xcom_push(
-        key='testing_increase',
-        value=testing_increase
+        key='df_abtest',
+        value=df
     )
 
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['example'])
-def taskflow_etl_pandas():
-    @task()
-    def extract():
-        order_data_dict = pd.DataFrame({
-            'a': np.random.rand(100000),
-            'b': np.random.rand(100000),
-        })
-        return order_data_dict
+def ab_test(ti):
+    """
+    Gets totalTestResultsIncrease field from Covid API for given state and returns value
+    """
+    df = ti.xcom_pull(
+        key='df_abtest',
+        task_ids='load_df'
+    )
 
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        return {"total_order_value": order_data_dict["a"].sum()}
+    ti.xcom_push(
+        key='df_abtest',
+        value=df
+    )
 
-    @task()
-    def load(total_order_value: float):
-        print(f"Total order value is: {total_order_value:.2f}")
-
-    order_data = extract()
-    order_summary = transform(order_data)
-    load(order_summary["total_order_value"])
 
 with DAG(
-    'xcom_dag',
-    start_date=datetime(2021, 1, 1),
+    'abtest_dag',
+    start_date=datetime(2022, 1, 1),
     max_active_runs=2,
     schedule_interval=timedelta(minutes=30),
     default_args=default_args,
